5ba2396f2177edf2e9aa321faceb638230080e21
jainshilpi <jainshilpi@users.noreply.github.com>
2024-02-07 17:15:37 +0530

    Add files via upload

--- myEMalgo.C
+++ myEMalgo.C
@@ -1,4 +1,3 @@
-/// Adapted from Satyaki Bhattacharya's code
 
 const int ncells = 60;
 double xmax = 30;
@@ -60,6 +59,96 @@ void myEMalgo(){
   }
 
 
+  double c[ncells];
+  double en[ncells];
+  
+  for(int ibin=1; ibin<=ncells; ibin++){
+
+    c[ibin-1] = h->GetBinCenter(ibin);
+    en[ibin-1] = h->GetBinContent(ibin);
+
+    //cout<<"ibin : binc : ene : "<<ibin<<" "<<c[ibin-1]<<" "<<en[ibin-1]<<endl;
+  }
+
+  //double binw = h->GetBinWidth(1);
+  //cout<<"binw "<<binw<<endl;
+  //h->Scale(binw);
+  
+
+  h->Draw();
+  
+
+  double f[100][100], a[100], mu[100];
+
+  for(int igaus=0; igaus<ngaus; igaus++){
+
+    ///it seems that assigning diff values to a and mu is imp else the result is same as the beginning
+    a[igaus] = 1*(igaus+10);
+    mu[igaus] = 1+igaus;
+  }
+
+  for(int iiter=0; iiter<niter; iiter++){
+    
+    //expectation
+    for(int icell=0; icell<ncells; icell++){
+    
+      double sum = 0;    
+      for(int igaus=0; igaus<ngaus; igaus++){
+	
+	//cout<<"igaus : mu : a "<<igaus<<" "<<mu[igaus]<<" "<<a[igaus]<<endl;
+	double arg = (c[icell] - mu[igaus])/sigma;
+
+	f[icell][igaus] = a[igaus]* TMath::Exp(-0.5*arg*arg);
+	
+	sum += f[icell][igaus];
+      }
+      
+      for(int igaus=0; igaus<ngaus; igaus++){
+	f[icell][igaus] = f[icell][igaus] /sum;
+	//cout<<"f["<<icell<<"]["<<igaus<<"] "<<f[icell][igaus]<<endl;
+      }
+      
+      
+    }//for(int icell=0; icell<ncells; icell++)
+  
+
+    //maximizationn 
+    for(int igaus=0; igaus<ngaus; igaus++){
+      
+      double a_tmp = 0, mu_tmp = 0;
+
+      for(int icell=0; icell<ncells; icell++){
+
+	
+	a_tmp += f[icell][igaus] * en[icell];  ///dont need to divide by sum of f's because norm of gaus is the sum of contribution from each cell
+	
+	mu_tmp += f[icell][igaus] * en[icell] * c[icell]; ///need to divide by sum of weights since this is the average of all the values
+	//cout<<"c : f : en : a : mu "<<c[icell]<< " "<<f[icell][igaus]<<" "<< en[icell]<<" "<<a_tmp<<" "<<mu_tmp<<endl;
+      }//for(int icell=0; icell<ncells; icell++)
+
+      //a_tmp = a_tmp;
+      mu_tmp = mu_tmp/a_tmp;
+      
+      cout<<"a_tmp : mu_tmp "<<a_tmp<<" "<<mu_tmp<<endl;
+      
+      a[igaus] = a_tmp;
+      mu[igaus] = mu_tmp;
+
+      f_est[igaus]->SetParameters(a[igaus],mu[igaus],sigma);
+
+      f_est[igaus]->Draw("same");
+      c1->Update();
+    }//for(int igaus=0; igaus<ngaus; igaus++)
+
+    c1->Print(Form("plot_iter%d.gif",iiter));
+    printf("\nDouble click in the bottom right corner of the pad to continue\n");
+    c1->WaitPrimitive();
+    
+
+  }//for(int iiter=0; iiter<niter; iiter++)
+
+  ///now try to estimate the Gaussian parameters with expectation maximization algorithm
+  
 
 }
 
